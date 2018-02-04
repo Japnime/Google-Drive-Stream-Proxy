40ae685b7ee32e45fad95f970b20efb12e890170
Threath <threathgfx@gmail.com>
2018-02-04 08:29:04 +0800

    Added new functions

--- class.DriveProxy.php
+++ class.DriveProxy.php
@@ -156,6 +156,12 @@
             $this->drive = $this->grab_video_content($proxylink, $cookie);
             return $this->drive;
         }
+
+        public function rangebyte($proxylink, $cookie)
+        {
+            $this->bytes = $this->grab_video_length($proxylink, $cookie);
+            return $this->bytes;
+        }
         
         private function grab_video_content($link, $cookie)
         {
@@ -180,7 +186,6 @@
     
             header("Content-Type: video/mp4");
             header("Content-length: ".$solution);
-            $timeset = 0;
             
             if (isset($_SERVER['HTTP_RANGE']))
             {
@@ -194,15 +199,43 @@
             {
                 header('HTTP/1.1 206 Partial Content');
                 header('Accept-Ranges: bytes'); 
-                header('Content-Range: bytes ' . $timeset . '-' . ($initial + $final) . '/' . $solution);
+                header('Content-Range: bytes ' . $initial . '-' . ($initial + $final) . '/' . $solution);
             } else {
                 header('Accept-Ranges: bytes'); 
             }
         }
         
+        private function grab_video_length($link, $cookie)
+        {
+            $curl = curl_init();
+            $payload = array(
+                CURLOPT_URL => $link,
+                CURLOPT_HEADER => true,
+                CURLOPT_USERAGENT => $this->useragent,
+                CURLOPT_CONNECTTIMEOUT => 0,
+                CURLOPT_TIMEOUT => 1000,
+                CURLOPT_FRESH_CONNECT => true,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_NOBODY => true,
+                CURLOPT_VERBOSE => 1,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_COOKIE => 'DRIVE_STREAM='.$cookie
+            );
+            curl_setopt_array($curl, $payload);
+            curl_exec($curl);
+            $solution = curl_getinfo($curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+            return $solution;
+        }
+
         public function stream()
         {
             echo $this->drive;
+        }
+        
+        public function result()
+        {
+            return $this->bytes;
         }
     }
 
