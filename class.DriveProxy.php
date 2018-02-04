e357be3542f16e46a58ebc5b4fa22f81058307be
Threath <threathgfx@gmail.com>
2018-02-04 08:30:11 +0800

    Typo update again

--- class.DriveProxy.php
+++ class.DriveProxy.php
@@ -149,6 +149,7 @@
     class DriveStream
     {
         private $drive = '';
+	private $bytes = '';
         private $useragent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36';
         
         public function ignition($proxylink, $cookie)
